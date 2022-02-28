1d7ab9e9ece10ec3013578b10d4bb5fa69807b88
Samuel Henrique Ferreira <samuelhenriqueferreira1@gmail.com>
2022-02-27 22:16:49 -0300

    adding gateway stepfunction construct

--- lib/masters-of-multiverse-stack.ts
+++ lib/masters-of-multiverse-stack.ts
@@ -1,16 +1,63 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { Effect, PolicyDocument, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 export class MastersOfMultiverseStack extends Stack {
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+  
+    // Flow that may be repeated by each entrypoint
 
-    // The code that defines your stack goes here
+    let entryFlowLambda = new lambda.Function(this, 'entryFLowLambdaHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda-fns'),
+      handler: 'entryPoint.handler'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MastersOfMultiverseQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Step functions are built up of steps, we need to define our first step
+    const entryPointJob = new tasks.LambdaInvoke(this, "Entry Point Job", {
+      lambdaFunction: entryFlowLambda,
+      inputPath: '$.flavour',
+      resultPath: '$.entryPointAnalysisResult',
+      payloadResponseOnly: true
+    })
+
+    // Failure step defined
+    const failStep = new sfn.Fail(this, 'Sorry, Failure on Execute Entrypoint', {
+      cause: 'Error Cause',
+      error: 'Error Description',
+    });
+
+    // Defined Success
+    const successStep = new sfn.Succeed(this, 'Lets make successStep', {
+      outputPath: '$.entryPointAnalysisResult'
+    });
+
+    //Express Step function definition
+    const definition = sfn.Chain
+    .start(entryPointJob)
+    .next(new sfn.Choice(this, 'With Choice?') // Logical choice added to flow
+        // Look at the "status" field
+        .when(sfn.Condition.booleanEquals('$.pineappleAnalysis.containsPineapple', true), failStep) // Fail First
+        .otherwise(successStep));
+
+    let stateMachine = new sfn.StateMachine(this, 'StateMachine', {
+      definition,
+      timeout: Duration.minutes(5),
+      tracingEnabled: true,
+      stateMachineType: sfn.StateMachineType.EXPRESS
+    });
+
+    //======  Endpoint Action is defined!!
+
+    const api = new apigateway.StepFunctionsRestApi(this, 'StepFunctionsRestApi', {
+      deploy: true,
+      stateMachine: stateMachine,
+    });
   }
 }
