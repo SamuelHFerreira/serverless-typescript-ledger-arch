39151d8adebc25d28d53e1cf6c4cc6e859b5605c
Samuel Henrique Ferreira <samuelhenriqueferreira1@gmail.com>
2022-03-13 23:18:21 -0300

    adding config load and ca interaction code

--- lib/masters-of-multiverse-stack.ts
+++ lib/masters-of-multiverse-stack.ts
@@ -1,6 +1,6 @@
 import { Stack, StackProps, Duration } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as path from 'path';
+import * as path from "path";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as sfn from "aws-cdk-lib/aws-stepfunctions";
@@ -99,16 +99,17 @@ export class MastersOfMultiverseStack extends Stack {
       }
     );
 
-    const handler: lambda.Function = new lambda.Function(this, 'GetUserLambdaHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset(sourceDir),
-      handler: 'getUser.handler',
-      timeout: Duration.seconds(10)
-    });
-
-    userResource.addMethod(
-      "GET",
-      new apigateway.LambdaIntegration(handler)
+    const handler: lambda.Function = new lambda.Function(
+      this,
+      "GetUserLambdaHandler",
+      {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        code: lambda.Code.fromAsset(sourceDir),
+        handler: "getUser.handler",
+        timeout: Duration.seconds(10),
+      }
     );
+
+    userResource.addMethod("GET", new apigateway.LambdaIntegration(handler));
   }
 }
