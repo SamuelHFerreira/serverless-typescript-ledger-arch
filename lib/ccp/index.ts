6d083d93d3bc25ffa10ba78f02d80721e2deeed5
Samuel Henrique Ferreira <samuelhenriqueferreira1@gmail.com>
2022-03-14 20:37:48 -0300

    format fix

--- lib/ccp/index.ts
+++ lib/ccp/index.ts
@@ -1,75 +1,74 @@
 // CA Configuration Properties
 export interface CCP {
-    name: string
-    version: string
-    client: Client
-    organizations: Organizations
-    peers: Peers
-    certificateAuthorities: CertificateAuthorities
-  }
-  
-  export interface Client {
-    organization: string
-    connection: Connection
-  }
-  
-  export interface Connection {
-    timeout: Timeout
-  }
-  
-  export interface Timeout {
-    peer: Peer
-  }
-  
-  export interface Peer {
-    endorser: string
-  }
-  
-  export interface Organizations {
-    "Org${ORG}": OrgOrg
-  }
-  
-  export interface OrgOrg {
-    mspid: string
-    peers: string[]
-    certificateAuthorities: string[]
-  }
-  
-  export interface Peers {
-    "peer0.org${ORG}.example.com": Peer0OrgOrgExampleCom
-  }
-  
-  export interface Peer0OrgOrgExampleCom {
-    url: string
-    tlsCACerts: TlsCacerts
-    grpcOptions: GrpcOptions
-  }
-  
-  export interface TlsCacerts {
-    pem: string
-  }
-  
-  export interface GrpcOptions {
-    "ssl-target-name-override": string
-    hostnameOverride: string
-  }
-  
-  export interface CertificateAuthorities {
-    [index: string]: CAOrg;
-  }
-  
-  export interface CAOrg {
-    url: string
-    caName: string
-    tlsCACerts: TlsCacerts2
-    httpOptions: HttpOptions
-  }
-  
-  export interface TlsCacerts2 {
-    pem: string[]
-  }
-  
-  export interface HttpOptions {
-    verify: boolean
-  }
-  
+  name: string;
+  version: string;
+  client: Client;
+  organizations: Organizations;
+  peers: Peers;
+  certificateAuthorities: CertificateAuthorities;
+}
+
+export interface Client {
+  organization: string;
+  connection: Connection;
+}
+
+export interface Connection {
+  timeout: Timeout;
+}
+
+export interface Timeout {
+  peer: Peer;
+}
+
+export interface Peer {
+  endorser: string;
+}
+
+export interface Organizations {
+  "Org${ORG}": OrgOrg;
+}
+
+export interface OrgOrg {
+  mspid: string;
+  peers: string[];
+  certificateAuthorities: string[];
+}
+
+export interface Peers {
+  "peer0.org${ORG}.example.com": Peer0OrgOrgExampleCom;
+}
+
+export interface Peer0OrgOrgExampleCom {
+  url: string;
+  tlsCACerts: TlsCacerts;
+  grpcOptions: GrpcOptions;
+}
+
+export interface TlsCacerts {
+  pem: string;
+}
+
+export interface GrpcOptions {
+  "ssl-target-name-override": string;
+  hostnameOverride: string;
+}
+
+export interface CertificateAuthorities {
+  [index: string]: CAOrg;
+}
+
+export interface CAOrg {
+  url: string;
+  caName: string;
+  tlsCACerts: TlsCacerts2;
+  httpOptions: HttpOptions;
+}
+
+export interface TlsCacerts2 {
+  pem: string[];
+}
+
+export interface HttpOptions {
+  verify: boolean;
+}
---
+++ test/masters-of-multiverse-stack.test.ts
@@ -0,0 +1,18 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as MastersOfMultiverse from "../lib/masters-of-multiverse-stack";
+
+test("Stack Tests", () => {
+  const app = new cdk.App();
+
+  const stack = new MastersOfMultiverse.MastersOfMultiverseStack(
+    app,
+    "MyTestStack"
+  );
+
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties("AWS::StepFunctions::StateMachine", {
+    StateMachineType: "EXPRESS",
+  });
+});
